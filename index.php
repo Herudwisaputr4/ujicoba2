8d04886ef5eed9b1d075c4e9696bfa2b887c43bf
Herudwisaputr4 <herudwisaputra017@gmail.com>
2025-03-22 21:09:50 +0700

    binding directory

--- index.php
+++ index.php
@@ -4,14 +4,20 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$system_dir = __DIR__."/system";
+
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+if (file_exists($maintenance = $system_dir.'/storage/framework/maintenance.php')) {
+    require $system_dir.'/storage/framework/maintenance.php';
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require $system_dir.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+(require_once $system_dir.'/bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+$app->bind('path.public', function(){
+    return __DIR__."/public";
+});
