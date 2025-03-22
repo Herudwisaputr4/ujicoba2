1f857b9d8e2669c27ca9c0a7cf9453c690462487
Herudwisaputr4 <herudwisaputra017@gmail.com>
2025-03-22 21:47:54 +0700

    routing laravel

--- system/routes/web.php
+++ system/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/deswita', function () {
+    return "Saya Deswita Mutia Putri";
+});
+
+Route::get('/jodoh', function () {
+    return "kami adalah jodoh";
+});
+
+Route::get('/heru', function () {
+    return "Saya Heru Dwi Saputra";
+});
